cb80e668d5508b0bb58f54929307bbce4e8e1873
BahiaSechi <bahia.s@outlook.fr>
2019-03-19 10:26:44 +0100

    premiere vue Films

--- app/src/main/java/com/example/e176688c/androidmovieproject/Films.java
+++ app/src/main/java/com/example/e176688c/androidmovieproject/Films.java
@@ -9,5 +9,6 @@ public class Films extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.films);
+
     }
 }
