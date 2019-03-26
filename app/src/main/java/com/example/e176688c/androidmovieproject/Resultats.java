32b17d277e058cc6d3576527b7b3c147f3f305a9
BahiaSechi <bahia.s@outlook.fr>
2019-03-26 10:15:48 +0100

    vues

--- app/src/main/java/com/example/e176688c/androidmovieproject/Resultats.java
+++ app/src/main/java/com/example/e176688c/androidmovieproject/Resultats.java
@@ -1,7 +1,20 @@
 package com.example.e176688c.androidmovieproject;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ScrollView;
 
 public class Resultats extends AppCompatActivity {
 
-}
+    private ScrollView scrollViewResults;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.resultats);
+
+        scrollViewResults = findViewById(R.id.scrollViewResults);
+
+    }
+
+    }
