db029019798cb4ea6a7e49d16b6c9fd6bfecafe8
BahiaSechi <bahia.s@outlook.fr>
2019-03-26 09:19:09 +0100

    film

--- app/src/main/java/com/example/e176688c/androidmovieproject/Films.java
+++ app/src/main/java/com/example/e176688c/androidmovieproject/Films.java
@@ -2,18 +2,20 @@ package com.example.e176688c.androidmovieproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Films extends AppCompatActivity {
 
     private EditText nomFilm;
     private TextView textViewDate;
     private Spinner genre;
-    private SeekBar seekBar;
+    private EditText number;
     private ImageView searchImage;
 
     @Override
@@ -24,8 +26,13 @@ public class Films extends AppCompatActivity {
         nomFilm = findViewById(R.id.recherche);
         textViewDate = findViewById(R.id.textViewDate);
         genre = findViewById(R.id.spinnerGenre);
-        seekBar = findViewById(R.id.seekBar);
+        number = findViewById(R.id.number);
         searchImage = findViewById(R.id.searchImage);
 
+        searchImage.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Vous avez cliqué sur l'image",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
