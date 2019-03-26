1d9cb5d04b5fd6891c8bfaf4d428842deea3fb66
BahiaSechi <bahia.s@outlook.fr>
2019-03-26 08:45:12 +0100

    film

--- app/src/main/java/com/example/e176688c/androidmovieproject/Films.java
+++ app/src/main/java/com/example/e176688c/androidmovieproject/Films.java
@@ -2,13 +2,30 @@ package com.example.e176688c.androidmovieproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class Films extends AppCompatActivity {
+
+    private EditText nomFilm;
+    private TextView textViewDate;
+    private Spinner genre;
+    private SeekBar seekBar;
+    private ImageView searchImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.films);
+
+        nomFilm = findViewById(R.id.recherche);
+        textViewDate = findViewById(R.id.textViewDate);
+        genre = findViewById(R.id.spinnerGenre);
+        seekBar = findViewById(R.id.seekBar);
+        searchImage = findViewById(R.id.searchImage);
 
     }
 }
